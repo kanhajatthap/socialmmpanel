45aa05f056f7ad6c27807754f44108100341c2cf
kanhajatthap <kanhajatthap@gmail.com>
2023-05-07 10:47:02 +0530

    first commit

---
+++ system/config_database.php
@@ -0,0 +1,20 @@
+<?php
+    
+    define('PATH', realpath('.'));
+    define('SUBFOLDER', false);
+    define('URL', 'https://socialmmpanel.com');
+    define('DINAMICLISANCE', 'GLYCON-THAZV-KGEYP-RMYOL');
+    
+    ini_set('display_errors', 0);
+    date_default_timezone_set('Europe/Istanbul');
+    
+    return [
+      'db' => [
+        'name'    =>  'socialmm_socialmmpanel',
+        'host'    =>  'localhost',
+        'user'    =>  'socialmm_socialmmpanel',
+        'pass'    =>  '~81hv)RefO$y',
+        'charset' =>  'utf8mb4' 
+      ]
+    ];
+    
